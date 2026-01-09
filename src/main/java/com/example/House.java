95a81c90604e958962f69a15f45e0cc24cb133ba
Eric Blanchette <ericblanchette55hockey@gmail.com>
2026-01-08 22:39:39 -0500

    added house functions

--- src/main/java/com/example/House.java
+++ src/main/java/com/example/House.java
@@ -1,3 +1,17 @@
+package com.example;
+
 public class House {
+    public int id;
+    public double price;
+    public String location;
     
+    public House(int id, double price, String location){
+        this.id = id;
+        this.price = price;
+        this.location = location;
+    }
+    
+    public void PrintDets(){
+        System.out.println("\nID: " + this.id + "\nPrice: " + this.price + "\nLocation: " + this.location);
+    }
 }
