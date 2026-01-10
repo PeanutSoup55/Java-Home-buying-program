5eefb146feb73e54c8666d8a750a3a6a2db453ab
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-01-09 21:46:44 -0500

    added menu function

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,12 +1,25 @@
 package com.example;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ericb
  */
 
 public class Main {
+    private Scanner s = new Scanner(System.in);
+
+    public static void displayMenu(){
+        int choice = 0;
+        System.out.println("==== Menu ====");
+        System.out.println("1. Make House");
+        System.out.println("2. Display Houses");
+        System.out.println("3. Make Realtor");
+        
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        displayMenu();
     }
 }
