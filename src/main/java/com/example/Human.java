7dfe88365f27b4bbdb84fdb921148a33406e6b7f
Eric Blanchette <ericblanchette55hockey@gmail.com>
2026-01-12 23:08:06 -0500

    added constructor and and base functions for human

--- src/main/java/com/example/Human.java
+++ src/main/java/com/example/Human.java
@@ -1,5 +1,23 @@
 package com.example;
 
 public class Human {
+    public int id;
+    public String name;
+    public int age;
+    public String phone;
     
+    public Human(int id, String name, int age, String phone){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+    
+    public void Introduce(){
+        System.out.println("Hi I'm " + this.name);
+    }
+    
+    public void Tour(){
+        System.out.println("Tour time");
+    }
 }
